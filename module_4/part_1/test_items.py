1bba9962e4833020d3bcfe6c2dd97ef567ad6f30
LineaRe <polina.reyner@gmail.com>
2020-12-04 21:24:10 +0300

    module_4 final

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -1,4 +1,5 @@
 #Arrange
+#Data
 main_page_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 basket_text_locator = "//div[2]/form/button"
 language_locator = "[selected='selected']"
@@ -16,8 +17,8 @@ def test_add_to_basket_btn(browser):
     exp_btn_text = exp_btn_text_dict[expected_lang_code]
 
     browser.get(main_page_link)
-
+#Act
     button_add_to_basket = browser.find_element_by_xpath(basket_text_locator)
     successful_button_text = button_add_to_basket.text
-
+#Assert
     assert exp_btn_text in successful_button_text, "Button text is incorrect"
